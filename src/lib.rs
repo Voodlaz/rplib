b625220d288ef1fc79f8385dcb1996040c1c3b47
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-07-06 21:59:36 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,6 @@
 use pyo3::prelude::*;
-use pyo3::{wrap_pyfunction,create_exception};
-use pyo3::exceptions::*;
-use pyo3::types::*;
-
-create_exception!(module, SomeError, Exception);
-
-#[pyfunction]
-fn arg(m: &PyAny) -> PyResult<&PyFloat> {
-    match m.downcast::<PyFloat>() {
-        Err(e) => println!("AAA"),
-        Ok(value) => Ok(value),
-    }
-}
 
 #[pymodule]
 fn rplib(py: Python, module: &PyModule) -> PyResult<()> {
-    module.add_wrapped(wrap_pyfunction!(arg))?;
     Ok(())
 }
