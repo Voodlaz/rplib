a9e2b814c04bd3f25e234963ef84f59eb148cb0a
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-07-06 18:20:00 +0300

    g

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,13 @@
 use pyo3::prelude::*;
-use pyo3::{wrap_pyfunction,create_exception};
+use pyo3::wrap_pyfunction;
 use pyo3::exceptions::*;
 use pyo3::types::*;
 
-create_exception!(module, MyError, Exception);
-
 #[pyfunction]
-fn arg(m: &PyAny) -> PyResult<&PyFloat> {
-    m.downcast::<PyFloat>().map_err(|_| MyError::py_err(()));
-
-    match m.downcast::<PyFloat>() {
-        Err(_) => Err(TypeError::py_err("AAAA")),
-        Ok(value) => value,
-    };
-}
+fn arg(m: &PyAny) -> PyResult<&PyFloat> {m}
 
 #[pymodule]
 fn rplib(py: Python, module: &PyModule) -> PyResult<()> {
     module.add_wrapped(wrap_pyfunction!(arg))?;
-
     Ok(())
 }
