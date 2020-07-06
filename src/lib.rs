f696161164eaaa7264a0433513148cf5bd0a921b
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-07-06 18:52:11 +0300

    н

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-()use pyo3::prelude::*;
+use pyo3::prelude::*;
 use pyo3::{wrap_pyfunction,create_exception};
 use pyo3::exceptions::*;
 use pyo3::types::*;
@@ -7,11 +7,9 @@ create_exception!(module, SomeError, Exception);
 
 #[pyfunction]
 fn arg(m: &PyAny) -> PyResult<&PyFloat> {
-    m;
-
-    match m.downcast::<PyFloat> {
+    match m.downcast::<PyFloat>() {
         Err(e) => println!("AAA"),
-        Ok(value) => value,
+        Ok(value) => value
     }
 }
 
